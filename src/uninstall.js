7539ffb873574705f55aa19f28bfb452cb959d22
brandonocasey <brandonocasey@gmail.com>
2020-11-09 16:29:46 -0500

    fix: remove npm-merge-driver too

--- src/uninstall.js
+++ src/uninstall.js
@@ -17,6 +17,12 @@ spawnSync(
   {cwd: rootDir}
 );
 
+spawnSync(
+  'git',
+  ['config', '--local', '--remove-section', 'merge.npm-merge-driver'],
+  {cwd: rootDir}
+);
+
 const attrFile = path.join(rootDir, '.git', 'info', 'attributes');
 
 // remove git attributes
@@ -25,11 +31,12 @@ if (fs.existsSync(attrFile)) {
 
   try {
     const RE = new RegExp('.* merge\\s*=\\s*npm-merge-driver-install$');
+    const RE2 = new RegExp('.* merge\\s*=\\s*npm-merge-driver$');
 
     attrContents = fs
       .readFileSync(attrFile, 'utf8')
       .split(/\r?\n/)
-      .filter(line => !line.match(RE))
+      .filter(line => !line.match(RE) && !line.match(RE2))
       .join('\n');
   } catch (e) {
     // some issue we cannot handle
